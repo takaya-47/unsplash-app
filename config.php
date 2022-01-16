46f94fd06f73ed305057e5f47cea737bd7e0497b
takaya-47 <local.s.s.47@gmail.com>
2022-01-16 10:23:00 +0900

    事前設定を追加

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+use Unsplash\HttpClient;
+
+/**
+ * アクセスキーとシークレットを使用してクライアントを構成します
+ * @return void
+ */
+function init(): void
+{
+  HttpClient::init([
+    'applicationId'  => $_ENV['UNSPLASH_ACCESS_KEY'],
+    'secret'  => $_ENV['UNSPLASH_SECRET_KEY'],
+    'callbackUrl'  => 'https://localhost:8080',
+    'utmSource' => 'beautifulPicituresApp'
+  ]);
+}
