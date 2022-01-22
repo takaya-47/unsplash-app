c31b7e2cb9b1779216069e56825884b443a9781c
takaya-47 <local.s.s.47@gmail.com>
2022-01-22 22:40:50 +0900

    検索条件の追加とロジックの変更

--- logic.php
+++ logic.php
@@ -1,6 +1,10 @@
 <?php
 use Unsplash\Search;
 
+const PAGE_NUMBER = 1;
+const PER_PAGE = 20;
+const ORIENTATION = 'landscape';
+
 /**
  * 検索ワードにより検索した結果を配列で返します
  * @param string  $search
@@ -8,6 +12,10 @@ use Unsplash\Search;
  */
 function search_photo($search): array
 {
-  $page_result = Search::photos($search);
-  return $page_result->getResults();
+  $page_result = Search::photos($search, PAGE_NUMBER, PER_PAGE, ORIENTATION, null, null);
+  if ($page_result->getTotal() === 0) {
+    return [];
+  } else {
+    return $page_result->getResults();
+  }
 }
