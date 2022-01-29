b29c4ddba60f5035493ff6881f97cd4bdf10d733
takaya-47 <local.s.s.47@gmail.com>
2022-01-29 16:49:08 +0900

    タグの記載ミスを修正

--- result.php
+++ result.php
@@ -38,7 +38,7 @@ $result_list = search_photos($search);
       <? } else { ?>
         <? foreach ($result_list as $result) { ?>
           <div class="img-box">
-            <img src="<?= $result['urls']['regular'] ?>" alt="検索結果" class="img-box__result-img" />
+            <img src="<?= $result['urls']['regular'] ?>" alt="検索結果" class="img-box__result-img" >
           </div>
         <? } ?>
       <? } ?>
