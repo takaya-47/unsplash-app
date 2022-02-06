195256b8d609a0040936e47f1fc1009e53a227a7
takaya-47 <local.s.s.47@gmail.com>
2022-02-06 16:18:37 +0900

    最終調整

--- result.php
+++ result.php
@@ -48,7 +48,6 @@ $result_list = search_photos($search);
   <div class="modal">
     <img src="" class="modal__image">
     <div class="modal__button">
-      <button class="modal__button__set">壁紙に設定</button>
       <button class="modal__button__close">閉じる</button>
     </div>
   </div>
